b9394486af89d529d2dfefad0b500b98a2535626
Hemanth Kollipara <defcon.sentinal95@gmail.com>
2020-02-22 11:46:57 +0530

    Add files via upload

---
+++ bot_control/motors.py
@@ -0,0 +1,46 @@
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BOARD)
+import time
+
+Motor1A = 31 # BACK_LEFT
+Motor1B = 33 # FRONT_RIGHT
+Motor2A = 35 # BACK_RIGHT
+Motor2B =  37# FRONT_LEFT
+
+GPIO.setup(Motor1A,GPIO.OUT)
+GPIO.setup(Motor1B,GPIO.OUT)
+GPIO.setup(Motor2A,GPIO.OUT)
+GPIO.setup(Motor2B,GPIO.OUT)
+
+def backward():
+	GPIO.output(Motor1A,GPIO.HIGH)
+	GPIO.output(Motor1B,GPIO.LOW)
+	GPIO.output(Motor2A,GPIO.HIGH)
+	GPIO.output(Motor2B,GPIO.LOW)
+
+def forward():
+	GPIO.output(Motor1A,GPIO.LOW)
+	GPIO.output(Motor1B,GPIO.HIGH)
+	GPIO.output(Motor2A,GPIO.LOW)
+	GPIO.output(Motor2B,GPIO.HIGH)
+
+def turnLeft():
+	print("Going Left")
+	GPIO.output(Motor1A,GPIO.HIGH)
+	GPIO.output(Motor1B,GPIO.LOW)
+	GPIO.output(Motor2A,GPIO.LOW)
+	GPIO.output(Motor2B,GPIO.HIGH)
+
+def turnRight():
+	print("Going Right")
+	GPIO.output(Motor1A,GPIO.LOW)
+	GPIO.output(Motor1B,GPIO.HIGH)
+	GPIO.output(Motor2A,GPIO.HIGH)
+	GPIO.output(Motor2B,GPIO.LOW)
+
+def stop():
+	print("Stopping")
+	GPIO.output(Motor1A,GPIO.LOW)
+	GPIO.output(Motor1B,GPIO.LOW)
+	GPIO.output(Motor2A,GPIO.LOW)
+	GPIO.output(Motor2B,GPIO.LOW)
